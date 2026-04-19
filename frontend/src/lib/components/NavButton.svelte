6ce007f55dbba6c85fdff8eb1ef92ad98ef47cf8
Simão Barbosa <simao.barbosa.05@gmail.com>
2026-04-19 15:06:05 +0100

    i fucking love this fucking nav bar

---
+++ frontend/src/lib/components/NavButton.svelte
@@ -0,0 +1,7 @@
+<script>
+    let { title, link, isClicked } = $props();
+</script> 
+
+<div class="px-4 py-1 min-w-20 text-center outline outline-black" class:bg-black={isClicked} class:text-white={isClicked}>
+    <a href={link} class=" hover:underline">{title}</a>
+</div>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import { resolve } from '$app/paths';
 	import { page } from '$app/state';
+	import NavButton from '$lib/components/NavButton.svelte';
 	
 	let { children } = $props();
 
@@ -18,12 +19,14 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="max-w-5xl mx-auto min-h-screen">	
-	<nav class="flex gap-4 px-4 py-12 mx-auto justify-around max-w-md">
-		<a href={resolve('/')} class:underline={isActive('/')}>Home</a>
-		<a href={resolve('/projects')} class:underline={isActive('/projects')}>Projects</a>
-		<a href={resolve('/blog')} class:underline={isActive('/blog')}>Blog</a>
-	</nav>
+<nav class="flex my-12 mx-auto justify-center max-w-md min-w-full border border-dotted">
+	<NavButton title="Home" link={resolve('/')} isClicked={isActive('/')} />
+	<NavButton title="Projects" link={resolve('/projects')} isClicked={isActive('/projects')} />
+	<NavButton title="Blog" link={resolve('/blog')} isClicked={isActive('/blog')} />
+</nav>
+
+<div class="max-w-5xl mx-auto min-h-screen">
+		
 	
 	{@render children()}
 </div>
