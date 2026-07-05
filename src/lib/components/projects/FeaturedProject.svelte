808d78cb8ce880e376c3e65d6fae6a67508f7e5a
Simão Barbosa <simao.barbosa.05@gmail.com>
2026-07-05 20:03:23 +0100

    better responsivity for projects page

--- src/lib/components/projects/FeaturedProject.svelte
+++ src/lib/components/projects/FeaturedProject.svelte
@@ -5,7 +5,7 @@
 <div class="flex flex-col items-center">
 	<div class="flex w-38 items-center justify-center">
 		<a href={link}
-			><img src={banner} alt="{title} banner" class="max-h-75 w-auto max-w-140 p-6" /></a
+			><img src={banner} alt="{title} banner" class="max-h-60 max-w-120 w-auto md:max-h-75 md:max-w-140 p-6" /></a
 		>
 	</div>
 	<div class="w-2/3 pb-6 text-center">
