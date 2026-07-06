85ef7e2911e949b906c73f77e6e23b456172f4fb
Simão Barbosa <simao.barbosa.05@gmail.com>
2026-07-06 12:47:42 +0100

    removed tags from blog posts

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -1,19 +1,12 @@
 <script>
 	import { resolve } from '$app/paths';
 
-	let { title, date, slug, tags = [] } = $props();
+	let { title, date, slug } = $props();
 </script>
 
 <div class="items-center justify-between py-2">
 	<h3><a class="" href={resolve('/blog/[slug]', { slug })}>{title}</a></h3>
-	<div class="my-0.5 flex gap-2">
+
 		<p class="secondary">{date}</p>
-		<div class="flex gap-1">
-			{#each Object.values(tags) as tag}
-				<span class="secondary bg-black px-1 pt-0.5 font-medium text-white!"
-					>{tag.toLowerCase()}</span
-				>
-			{/each}
-		</div>
-	</div>
+	
 </div>
