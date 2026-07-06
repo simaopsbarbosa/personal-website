24caf1399ade75daf8e836870b7b683d9612d49f
Simão Barbosa <simao.barbosa.05@gmail.com>
2026-07-07 00:07:27 +0100

    configure cloudflare pages adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { relative, sep } from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -17,13 +17,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html', // Essential for SPAs or custom error pages
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter()
 	}
 };
 
