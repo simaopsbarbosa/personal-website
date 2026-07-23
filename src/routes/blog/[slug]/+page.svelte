d2dc32846621a0237462de3dbec1bd8a25cfd4eb
Simão Barbosa <simao.barbosa.05@gmail.com>
2026-07-23 17:56:13 +0100

    changed age at writing to be less confusing

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -6,11 +6,15 @@
 
 <h2>{data.post.title}</h2>
 <p class="secondary pb-6">
-	{formatDate(data.post.created_at)} | {calculateAge(data.post.created_at).formatted} old
+	{formatDate(data.post.created_at)}
 </p>
 
 <div class="prose max-w-none">
 	{@html data.post.content}
+</div>
+
+<div class="secondary centered pt-4">
+	At the time of writing, I was {calculateAge(data.post.created_at).formatted} old
 </div>
 
 <style>
